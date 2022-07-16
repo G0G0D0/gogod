d13c241493c518f94a09e77adaab90d116383631
G0G0D0 <95535369+G0G0D0@users.noreply.github.com>
2022-07-16 18:21:44 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/yashwant/Gogodo/MainActivity.kt
@@ -0,0 +1,35 @@
+ package com.yashwant.Gogodo
+ //package com.muhammadali.telegramchatapp;
+
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
+
+
+
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        // TODO: Register the ListTileNativeAdFactory
+        GoogleMobileAdsPlugin.registerNativeAdFactory(
+            flutterEngine, "listTile",
+            com.yashwant.Gogodo.ListTileNativeAdFactory(context)
+        )
+    }
+
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
+        super.cleanUpFlutterEngine(flutterEngine)
+
+        // TODO: Unregister the ListTileNativeAdFactory
+        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "listTile")
+    }
+
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//    }
+}
